c72ac86ea90e4bf686857f25c49e001a667552a3
Aditya <adityapawar826@gmail.com>
2025-06-26 20:20:42 +0530

    Pipes in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { StudentService } from './Services/student.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AdminComponent } from './admin/admin.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [StudentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
